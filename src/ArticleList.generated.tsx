a6e78aa1952052e5c4088d941772e5b3fe1a5ac1
mizdra <pp.mizdra@gmail.com>
2019-10-04 15:55:40 +0900

    upgrade v1.8.0

--- src/ArticleList.generated.tsx
+++ src/ArticleList.generated.tsx
@@ -1,10 +1,15 @@
+import * as Types from './types.generated';
+
 import { SimpleArticleFragment } from './ArticleListItem.generated';
+
+
 export type GetArticlesQueryVariables = {};
 
 
 export type GetArticlesQuery = (
   { __typename?: 'Query' }
-  & { articles: Array<{ __typename?: 'Article' }
+  & { articles: Array<(
+    { __typename?: 'Article' }
     & SimpleArticleFragment
-  > }
+  )> }
 );
--- src/ArticleListItem.generated.tsx
+++ src/ArticleListItem.generated.tsx
@@ -1,5 +1,6 @@
 import * as Types from './types.generated';
 
+
 export type SimpleArticleFragment = (
   { __typename?: 'Article' }
   & Pick<Types.Article, 'title' | 'body'>
